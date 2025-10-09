849c7fb52df8106c8796bdc77039dc460c71d036
Sahelluis21 <Sahelluis77@gmail.com>
2025-10-08 23:48:09 -0300

    Tentando Resolver Composer - Laravel

--- php/src/public/Index.php
+++ php/src/public/Index.php
@@ -1,29 +1,17 @@
 <?php
-// public/index.php
-session_start();
 
-$uri = $_SERVER['REQUEST_URI'];
+define('LARAVEL_START',microtime(true));
 
-if ($uri === '/login') {
-    require_once __DIR__ . '/../controller/authcontroller.php';
-    $controller = new AuthController();
-    $controller->login();
-} elseif ($uri === '/logout') {
-    require_once __DIR__ . '/../controller/authcontroller.php';
-    $controller = new AuthController();
-    $controller->logout();
-} else {
-    if (!isset($_SESSION['user_id'])) {
-        header("Location: /auth/login.php");
-        exit;
-    }
-    require_once __DIR__ . '/../controller/homecontroller.php';
-    $controller = new HomeController();
-    $controller->index();
-}
-if ($uri === '/upload' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    require_once __DIR__ . '/../class/controlador.php';
-    $controlador = new Controlador($conn);
-    $result = $controlador->upload($_FILES,$userId);
-    exit;
-}
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
